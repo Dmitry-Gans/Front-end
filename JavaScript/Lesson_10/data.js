38f2914553690d10fba07505db5662cde6b1a192
Dmitry <dmitry.guslov-gans@yandex.ru>
2024-04-11 20:40:06 +0300

    Закончил 10 урок

---
+++ JavaScript/Lesson_10/data.js
@@ -0,0 +1,32 @@
+// Задание 2 (тайминг 15 минут)
+// 1. Создать файл data.js
+// 2. Создать переменную dataInfo
+// 3. Добавить несколько данных в dataInfo
+
+const dataInfo = `
+[
+
+    {
+        "class": "img__container",
+        "url": "https://gas-kvas.com/grafic/uploads/posts/2023-10/1696502271_gas-kvas-com-p-kartinki-lyubie-5.jpg",
+        "width": "250",
+        "height": "250",
+        "text": "Картинка 1"
+    },
+    {
+        "class": "img__container",
+        "url": "https://w.forfun.com/fetch/73/73aa25921c66f5a77e44d6e653ebc33b.jpeg",
+        "width": "350",
+        "height": "350",
+        "text": "Картинка 2"
+    },
+    {
+        "class": "img__container",
+        "url": "https://w.forfun.com/fetch/71/71f0c755f4b257fb9987c25743d16388.jpeg",
+        "width": "500",
+        "height": "500",
+        "text": "Картинка 3"
+    }
+
+]
+`;
---
+++ JavaScript/Lesson_10/script.js
@@ -0,0 +1,73 @@
+// Задание 3 (тайминг 30 минут)
+// 1. Создать файл index.html
+// 2. Подключить data.js
+// 3. Добавить блок <div class="content"></div>
+// 4. Создать переменную data и добавить в нее JSON parse данные из
+// файла data.js
+// 5. Вывести в консоль объект data
+// 6. С помощью foreach обойти массив data
+// 7. Вывести все изображения из данных
+
+// const parseResult = JSON.parse(dataInfo);
+
+// const divContEl = document.querySelector("div.container");
+
+// parseResult.forEach((element) => {
+//   const newDivEl = document.createElement("div");
+//   newDivEl.classList.add(element.class);
+
+//   const imgEl = document.createElement("img");
+//   imgEl.setAttribute("src", element.url);
+//   imgEl.setAttribute("width", element.width);
+//   imgEl.setAttribute("height", element.height);
+
+//   Задание 4 (тайминг 20 минут)
+// 1. Создать все необходимые заголовки, параграфы изображения (из
+// данных json)
+// 2. Добавить все содержимое в блок контент
+// 3. Добавить стили при необходимости
+
+//   const textEl = document.createElement("p");
+//   textEl.innerHTML = element.text;
+
+//   newDivEl.appendChild(imgEl);
+//   newDivEl.appendChild(textEl);
+//   divContEl.appendChild(newDivEl);
+// });
+
+// Дополнительный вариант с запросом с сервера:
+
+const divContEl = document.querySelector('div.container');
+
+fetch("https://api.nbrb.by/exrates/rates?periodicity=0")
+    .then((response) => response.json())
+    .then((parseResult) => {
+        parseResult.forEach(element => {
+            // console.log(element);
+            const newDivEl = document.createElement('div');
+            newDivEl.classList.add('img__container');
+
+            const textData = document.createElement('p');
+            textData.innerHTML = element.Date;
+            newDivEl.appendChild(textData);
+            
+            const textCurAbb = document.createElement('p');
+            textCurAbb.innerHTML = element.Cur_Abbreviation;
+            newDivEl.appendChild(textCurAbb);
+
+            const textCurScale = document.createElement('p');
+            textCurScale.innerHTML = element.Cur_Scale;
+            newDivEl.appendChild(textCurScale);
+
+            const textCurName = document.createElement('p');
+            textCurName.innerHTML = element.Cur_Name;
+            newDivEl.appendChild(textCurName);
+
+            const textOffRate = document.createElement('p');
+            textOffRate.innerHTML = element.Cur_OfficialRate;
+            newDivEl.appendChild(textOffRate);
+
+            divContEl.appendChild(newDivEl);
+
+        });
+    });
