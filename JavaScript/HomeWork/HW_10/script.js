8c3568bb47933f56ce2ffc6c9246f198c20fe8e3
Dmitry <dmitry.guslov-gans@yandex.ru>
2024-04-14 22:29:44 +0300

    Сделал 10 ДЗ

---
+++ JavaScript/HomeWork/HW_10/script.js
@@ -0,0 +1,39 @@
+// Задание 1
+// 1. Поиск в интернете (бесплатные api примеры).
+// 2. Найти любые данные, на произвольную тему.
+// 3. Создать файл data.js.
+// 4. Создать переменную которая будет хранить данные из публичных api.
+
+const parseResult = JSON.parse(catsData);
+console.log(parseResult);
+
+// Задание 2
+// 1. Создать файл index.html.
+// 2. Подключить data.js.
+// 3. Сформировать контент из данных (картинка загловок и параграф).
+// 4. Добавить данный контент в вёрстку.
+// 5. * Добавить стили при необходимости (по желанию).
+
+const divEl = document.querySelector('div.container');
+
+parseResult.forEach(e => {
+    const itemEl = document.createElement('div');
+    itemEl.classList.add('item');
+    divEl.appendChild(itemEl);
+
+    const authorEl = document.createElement('h1');
+    authorEl.classList.add('author');
+    authorEl.innerHTML = "Автор: " + e.author;
+    itemEl.appendChild(authorEl);
+    
+    const quoteEl = document.createElement('h2');
+    quoteEl.classList.add('quote');
+    quoteEl.innerHTML = "Цитата: " + e.quote;
+    itemEl.appendChild(quoteEl);
+
+    const seriesEl = document.createElement('p');
+    seriesEl.classList.add('series');
+    seriesEl.innerHTML = "Серия книг: " + e.series;
+    itemEl.appendChild(seriesEl);
+
+});
