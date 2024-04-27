2f5294b94c2a5a00118eaa3f1ef659358202bb81
Dmitry <dmitry.guslov-gans@yandex.ru>
2024-04-28 00:32:50 +0300

    Добавил функционал: счетчик товаров в виде иконки над корзиной и удаление товаров

--- Web_HTML_CSS/HomeWork/HW_6/script/dataProductsItem.js
+++ Web_HTML_CSS/HomeWork/HW_6/script/dataProductsItem.js
@@ -5,7 +5,9 @@ const dataProductsItem = `[
         "textButton":"Add to Cart",
         "title": "ELLERY X M'O CAPSULE",
         "textContent":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        "price": "$52.99"
+        "price":"$52.99",
+        "color":"White",
+        "size":"XL"
     },
     {
         "class":"products-content__item",
@@ -13,7 +15,9 @@ const dataProductsItem = `[
         "textButton":"Add to Cart",
         "title": "ELLERY X M'O CAPSULE",
         "textContent":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        "price": "$64.00"
+        "price": "$64.00",
+        "color":"Black",
+        "size":"X"
     },
     {
         "class":"products-content__item",
@@ -21,7 +25,9 @@ const dataProductsItem = `[
         "textButton":"Add to Cart",
         "title": "ELLERY X M'O CAPSULE",
         "textContent":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        "price": "$37.00"
+        "price": "$37.00",
+        "color":"Blue",
+        "size":"S"
     },
     {
         "class":"products-content__item",
@@ -29,7 +35,9 @@ const dataProductsItem = `[
         "textButton":"Add to Cart",
         "title": "ELLERY X M'O CAPSULE",
         "textContent":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        "price": "$29.99"
+        "price": "$29.99",
+        "color":"Gray",
+        "size":"XL"
     },
     {
         "class":"products-content__item",
@@ -37,7 +45,9 @@ const dataProductsItem = `[
         "textButton":"Add to Cart",
         "title": "ELLERY X M'O CAPSULE",
         "textContent":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        "price": "$72.99"
+        "price": "$72.99",
+        "color":"Blue",
+        "size":"L"
     },
     {
         "class":"products-content__item",
@@ -45,7 +55,9 @@ const dataProductsItem = `[
         "textButton":"Add to Cart",
         "title": "ELLERY X M'O CAPSULE",
         "textContent":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        "price": "$18.00"
+        "price": "$18.00",
+        "color":"Green",
+        "size":"L"
     }
 
 ]`;
--- Web_HTML_CSS/HomeWork/HW_6/script/script.js
+++ Web_HTML_CSS/HomeWork/HW_6/script/script.js
@@ -1,20 +1,98 @@
 "use strict";
 
-const dataProducts = JSON.parse(dataProductsItem);
-const articleEl = document.querySelector('article.products-content');
+const dataProducts = JSON.parse(dataProductsItem); // Парсим JSON файл
+const articleProductEl = document.querySelector("article.products-content");
 
-dataProducts.forEach(element => {
-    const templateEl = document.getElementById('cartTemplate');
-    const clone = templateEl.content.cloneNode(true);
+//Создание карточек продукта:
+dataProducts.forEach((element) => {
+  // Перебираем каждый элемент в JSON файле
+  const templateEl = document.getElementById("cartTemplate"); // Находим шаблон по индексу
+  const clone = templateEl.content.cloneNode(true); // Делаем копию шаблона
 
-    clone.querySelector('img.products-content__img-bg').src = element.src;
-    clone.querySelector('p.products-content__button_text').innerHTML = element.textButton;
-    clone.querySelector('h2.products-content__title').innerHTML = element.title;
-    clone.querySelector('p.products-content__text').innerHTML = element.textContent;
-    clone.querySelector('p.products-content__price').innerHTML = element.price;
+  // Заполняем шаблон с помощью полей JSON файла
+  clone.querySelector("img.products-content__img-bg").src = element.src;
+  clone.querySelector("p.products-content__button_text").innerHTML =
+    element.textButton;
+  clone.querySelector("h2.products-content__title").innerHTML = element.title;
+  clone.querySelector("p.products-content__text").innerHTML =
+    element.textContent;
+  clone.querySelector("p.products-content__price").innerHTML = element.price;
 
-
-    articleEl.appendChild(clone);
+  articleProductEl.appendChild(clone); // Добавляем готовый шаблон внутрь нужного родителя
 });
 
+// Переменные для взаимодействия с корзиной:
+const articleCartPreview = document.querySelector("article.cartPreview");
+const divCartBoxEl = document.querySelector("div.cart-content__box");
+const divCartEl = document.querySelector("div.cart-content__selected");
+const buttonAddToCart = document.querySelectorAll("a.products-content__button");
+const titleCart = divCartBoxEl.querySelector("h2.cart-content__box-title");
+const counterValue = document.querySelector("p.nav__number");
 
+// Счетчик сколько товаров в корзине над иконкой "Корзина":
+buttonAddToCart.forEach((e) => {
+  // Перебираем каждую кнопку и накладываем event
+  e.addEventListener("click", (e) => {
+    const number =
+      divCartEl.querySelectorAll("div.cart-content__selected-item").length + 1; // Создаем переменную для отслеживания, она следит, сколько товаров в корзине, массив начинается с 0, поэтому + 1
+    counterValue.classList.remove("none");
+    counterValue.innerHTML = number; // Присваиваем количество  товаров иконке
+  });
+});
+
+//Добавление карточек в корзину:
+buttonAddToCart.forEach((e, index) => {
+  // Перебираем все кнопки и накладываем event
+  e.addEventListener("click", (e) => {
+    const templateCartEl = document.getElementById("templateCartItem");
+    const cloneCart = templateCartEl.content.cloneNode(true);
+
+    articleCartPreview.classList.remove("none");
+    titleCart.classList.remove("none");
+    cloneCart.querySelector("img.cart-content__img").src =
+      dataProducts[index].src;
+    cloneCart.querySelector("p.cart-content__title").innerHTML =
+      dataProducts[index].title;
+    cloneCart.querySelector("span.text-price").innerHTML =
+      dataProducts[index].price;
+    cloneCart.querySelector("span.text-colors").innerHTML =
+      dataProducts[index].color;
+    cloneCart.querySelector("span.text-size").innerHTML =
+      dataProducts[index].size;
+
+    divCartEl.appendChild(cloneCart);
+  });
+});
+
+//Удаление карточек из корзины:
+divCartEl.addEventListener("click", (e) => {
+  // Захватываем всю область после заголовка Корзины
+  if (e.target.closest(".cart-content__button")) {
+    // Делегируем event только наследнику(Кнопка удалить/закрыть)
+    const deleteItem = e.target
+      .closest(".cart-content__heading")
+      .closest(".cart-content__info")
+      .closest(".cart-content__selected-item"); // При нажатии на кнопку Удалить, ищем по вложенности родительский элемент всей карточки
+
+    deleteItem.remove(); // Удаляем родительский элемент(Карточку)
+
+    counterValue.innerHTML -= 1; // Убавляем счетчик у верхней иконки у корзины
+
+    // Проверка у иконки, если у иконки будет 0, то скрываем всю иконку
+    if (counterValue.innerHTML.includes("0")) {
+      counterValue.classList.add("none");
+    }
+  }
+
+  // Вызываем метод на проверки наличия товаров в корзине
+  checkCart();
+});
+
+// Метод, проверяющий корзину, если она пуста, то скрываем весь блок Предпросмотра корзины
+const checkCart = () => {
+  if (
+    divCartEl.querySelectorAll("div.cart-content__selected-item").length === 0
+  ) {
+    articleCartPreview.classList.add("none");
+  }
+};
