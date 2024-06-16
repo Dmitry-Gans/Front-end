3f401bb5009b9505e3d0c98e6d9f256a4594654d
Dmitry <dmitry.guslov-gans@yandex.ru>
2024-06-16 21:30:46 +0300

    Доработал 2 задачу

--- Advanced_JavaScript/HomeWorks/HW_1/task2.js
+++ Advanced_JavaScript/HomeWorks/HW_1/task2.js
@@ -29,59 +29,109 @@
 // Клиент Мария заказала: Суши "Калифорния" и Пиццу "Маргарита".
 // Клиент Ирина заказала: Чизкейк.
 
-const listOfChefs = new Map();
-const listOfOrders = new Map();
+// Создаем коллекцию Map для хранения блюд и их поваров
+const dishesAndChefs = new Map([
+  ['Пицца "Маргарита"', "Виктор"],
+  ['Пицца "Пепперони"', "Виктор"],
+  ['Суши "Филадельфия"', "Ольга"],
+  ['Суши "Калифорния"', "Ольга"],
+  ["Тирамису", "Дмитрий"],
+  ["Чизкейк", "Дмитрий"],
+]);
 
-const mealVictor = {
-  chef: "Виктор",
-  meals: new Set(["Пепперони", "Маргарита"]),
-};
-const mealOlga = {
-  chef: "Ольга",
-  meals: new Set(["Калифорния", "Анигири"]),
-};
-const mealDmitry = {
-  chef: "Дмитрий",
-  meals: new Set(["Тирамису", "Чизкейк"]),
-};
+// Создаем коллекцию Map для хранения заказов каждого клиента
+const orders = new Map();
 
-listOfChefs.set("Пицца", mealVictor);
-listOfChefs.set("Суши", mealOlga);
-listOfChefs.set("Десерты", mealDmitry);
-
-class Order {
-  constructor(client, type, meal) {
-    this.client = client;
-    this.type = type;
-    this.meal = meal;
-  }
-  confirmOrder() {
-    if (listOfOrders.has(this.client)) {
-      if (
-        listOfChefs.has(this.type) &&
-        listOfChefs.get(this.type).meals.has(this.meal)
-      ) {
-        listOfOrders.get(this.client).add(this.meal);
-      } else {
-        return "Нет такого блюда";
-      }
+// Функция для добавления заказа клиента
+function addOrder(client, dish) {
+  if (orders.has(client)) {
+    if (dishesAndChefs.has(dish)) {
+      orders.get(client).push(dish);
     } else {
-      listOfOrders.set(this.client, new Set([this.meal]));
+      console.error(`${client}, нет такого блюда: ${dish}!`);
     }
-    const nameChef = listOfChefs.get(this.type).chef;
-    const meals = [...listOfOrders.get(this.client)].join(", ");
-    return `Клиент ${this.client} заказал: ${meals}, его готовит ${nameChef}`;
+  } else {
+    if (dishesAndChefs.has(dish)) {
+      orders.set(client, [dish]);
+    } else {
+      console.error(`${client}, нет такого блюда: ${dish}!`);
+    }
   }
 }
 
-const order = new Order("Саша", "Пицца", "Пепперони");
-const order2 = new Order("Саша", "Десерты", "Чизкейк");
-const order3 = new Order("Саша", "Суши", "выаыв");
-const order4 = new Order("Петя", "Суши", "Калифорния");
+// Заказы клиентов
+addOrder("Алексей", 'Пицца "Пепперони"');
+addOrder("Алексей", "sdfsd");
 
-console.log(order.confirmOrder());
-console.log(order2.confirmOrder());
-console.log(order3.confirmOrder());
-console.log(order4.confirmOrder());
+addOrder("Мария", 'Суши "Калифорния"');
+addOrder("Мария", 'Пицца "Маргарита"');
 
-console.log(listOfOrders);
+addOrder("Ирина", "Чизкейк");
+
+// Вывод информации о заказах
+orders.forEach((dishes, client) => {
+  console.log(`${client} заказал(а):`);
+  dishes.forEach((dish) => {
+    console.log(`- ${dish}`);
+    console.log(`  (готовит: ${dishesAndChefs.get(dish)})`);
+  });
+  console.log("");
+});
+
+// Проба пера:
+// const listOfChefs = new Map();
+// const listOfOrders = new Map();
+
+// const mealVictor = {
+//   chef: "Виктор",
+//   meals: new Set(["Пепперони", "Маргарита"]),
+// };
+// const mealOlga = {
+//   chef: "Ольга",
+//   meals: new Set(["Калифорния", "Анигири"]),
+// };
+// const mealDmitry = {
+//   chef: "Дмитрий",
+//   meals: new Set(["Тирамису", "Чизкейк"]),
+// };
+
+// listOfChefs.set("Пицца", mealVictor);
+// listOfChefs.set("Суши", mealOlga);
+// listOfChefs.set("Десерты", mealDmitry);
+
+// class Order {
+//   constructor(client, type, meal) {
+//     this.client = client;
+//     this.type = type;
+//     this.meal = meal;
+//   }
+//   confirmOrder() {
+//     if (listOfOrders.has(this.client)) {
+//       if (
+//         listOfChefs.has(this.type) &&
+//         listOfChefs.get(this.type).meals.has(this.meal)
+//       ) {
+//         listOfOrders.get(this.client).add(this.meal);
+//       } else {
+//         return "Нет такого блюда";
+//       }
+//     } else {
+//       listOfOrders.set(this.client, new Set([this.meal]));
+//     }
+//     const nameChef = listOfChefs.get(this.type).chef;
+//     const meals = [...listOfOrders.get(this.client)].join(", ");
+//     return `Клиент ${this.client} заказал: ${meals}, его готовит ${nameChef}`;
+//   }
+// }
+
+// const order = new Order("Саша", "Пицца", "Пепперони");
+// const order2 = new Order("Саша", "Десерты", "Чизкейк");
+// const order3 = new Order("Саша", "Суши", "выаыв");
+// const order4 = new Order("Петя", "Суши", "Калифорния");
+
+// console.log(order.confirmOrder());
+// console.log(order2.confirmOrder());
+// console.log(order3.confirmOrder());
+// console.log(order4.confirmOrder());
+
+// console.log(listOfOrders);
