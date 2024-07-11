589ec015ecc1b83889d759faad741c85974575da
Dmitry <dmitry.guslov-gans@yandex.ru>
2024-07-11 15:28:56 +0300

    Подправил задачу

--- API_JavaScript/Lesson_3/task1/index.js
+++ API_JavaScript/Lesson_3/task1/index.js
@@ -1,61 +1,6 @@
-// Задача 1.
+'use strict'
 
-// Дан массив товаров:
-
-// ```
-productsData = [
-  {
-    id: 1,
-    name: "Ноутбук",
-    category: "Электроника",
-  },
-  {
-    id: 2,
-    name: "Смартфон",
-    category: "Электроника",
-  },
-  {
-    id: 3,
-    name: "Кофемашина",
-    category: "Бытовая техника",
-  },
-  {
-    id: 4,
-    name: "Фотокамера",
-    category: "Электроника",
-  },
-  {
-    id: 5,
-    name: "Микроволновая печь",
-    category: "Бытовая техника",
-  },
-  {
-    id: 6,
-    name: "Книга",
-    category: "Книги",
-  },
-  {
-    id: 7,
-    name: "Футболка",
-    category: "Одежда",
-  },
-  {
-    id: 8,
-    name: "Шапка",
-    category: "Одежда",
-  },
-  {
-    id: 9,
-    name: "Стул",
-    category: "Мебель",
-  },
-  {
-    id: 10,
-    name: "Стол",
-    category: "Мебель",
-  },
-];
-// ```
+// Задача 1
 
 // Вы разрабатываете интернет-магазин и хотите добавить
 // функциональность динамической фильтрации товаров по
@@ -81,25 +26,79 @@ productsData = [
 // Стилизуйте элементы интерфейса с помощью CSS для улучшения
 // внешнего вида.
 
+const productsData = [
+	{
+		id: 1,
+		name: 'Ноутбук',
+		category: 'Электроника',
+	},
+	{
+		id: 2,
+		name: 'Смартфон',
+		category: 'Электроника',
+	},
+	{
+		id: 3,
+		name: 'Кофемашина',
+		category: 'Бытовая техника',
+	},
+	{
+		id: 4,
+		name: 'Фотокамера',
+		category: 'Электроника',
+	},
+	{
+		id: 5,
+		name: 'Микроволновая печь',
+		category: 'Бытовая техника',
+	},
+	{
+		id: 6,
+		name: 'Книга',
+		category: 'Книги',
+	},
+	{
+		id: 7,
+		name: 'Футболка',
+		category: 'Одежда',
+	},
+	{
+		id: 8,
+		name: 'Шапка',
+		category: 'Одежда',
+	},
+	{
+		id: 9,
+		name: 'Стул',
+		category: 'Мебель',
+	},
+	{
+		id: 10,
+		name: 'Стол',
+		category: 'Мебель',
+	},
+]
+
+const catalogEl = document.querySelector('.wrapper')
+const categorySelectEl = document.querySelector('#categorySelect')
+categorySelectEl.addEventListener('change', function () {
+	const selectCategory = categorySelectEl.value
+	displayCatalog(selectCategory)
+})
+
 function displayCatalog(selectCategory) {
-  let productHTML = "";
-  productsData.forEach((element) => {
-    if (selectCategory === "" || element.category === selectCategory) {
-      productHTML += createCard(element);
-    }
-  });
-  catalogEl.innerHTML = productHTML;
+	let productHTML = ''
+	productsData.forEach(element => {
+		if (selectCategory === '' || element.category === selectCategory) {
+			productHTML += createCard(element)
+		}
+	})
+	catalogEl.innerHTML = productHTML
 }
 function createCard(cardInfo) {
-  return `<div class="card">
+	return `<div class="card">
           <div class="card__h1">${cardInfo.id}</div>
           <div class="card__name">${cardInfo.name}</div>
           <div class="card__category">${cardInfo.category}</div>
-        </div>`;
+        </div>`
 }
-const catalogEl = document.querySelector(".wrapper");
-const categorySelectEl = document.querySelector("#categorySelect");
-categorySelectEl.addEventListener("change", function () {
-  const selectCategory = categorySelectEl.value;
-  displayCatalog(selectCategory);
-});
